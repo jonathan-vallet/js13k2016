35dd6817a0dfc2cfae71cbca4f1636e9e512cba2
jonathan-vallet <jonathan.vallet@adfab.fr>
2016-08-22 16:24:40 +0200

    Added config / gulp

---
+++ gulp-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    vhost: 'www.js13k2016.jvallet.dev',
+
+	source: {
+        cssWatchFileList: [ './src/assets/less/**/*.less' ],
+        cssCompileFileList: [ './src/assets/less/style.less' ],
+        libFileList: [ './src/assets/scripts/lib/*.js' ],
+        jsEntryFile: [ './src/assets/scripts/app.js' ],
+        imageFileList: [ './src/assets/images/**/*' ],
+        viewFileList: [ './src/assets/views/**/*.html' ],
+        fontFileList: [ './src/assets/fonts/**/*' ],
+        fontIconFileList: [ './src/assets/icons/*.svg'],
+	},
+
+	destination: {
+	    assetsFolder: './www/', // General assets destination path. 
+	    cssFolderName: 'css', // Name of the folder where put css file in asset folder.
+	    cssFileName: 'style.css',
+        libFolderName: 'scripts',
+        libFileName: 'lib.js',
+        jsFolderName: 'scripts',
+        jsFileName: 'app.js',
+        imagesFolderName: 'images',
+	    viewsFolderName: '',
+        fontsFolderName: 'fonts',
+        fontIconFontName: 'fonticon',
+        fontIconFontFolderFile: './src/assets/fonts/',
+        fontIconLessFile: '../less/common/_fonticon.less'
+	}
+};
---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var config      = require('./gulp-config');
+var runSequence = require('run-sequence');
+var gulp        = require('gulp');
+var del         = require('del');
+var browserSync  = require('browser-sync');
+
+gulp.task('scripts', require('./gulp-tasks/scripts'));
+gulp.task('styles', require('./gulp-tasks/less'));
+gulp.task('images', require('./gulp-tasks/images'));
+gulp.task('fonts', require('./gulp-tasks/fonts'));
+gulp.task('lib', require('./gulp-tasks/lib'));
+gulp.task('views', require('./gulp-tasks/views'));
+gulp.task('fonticon', require('./gulp-tasks/fonticon'));
+
+/**
+ * Clean build directory
+ */
+gulp.task('clean', function(cb) {
+    del([config.destination.assetsFolder + config.destination.fontFolderName,
+        config.destination.assetsFolder + config.destination.cssFolderName,
+        config.destination.assetsFolder + config.destination.libFolderName,
+        config.destination.assetsFolder + config.destination.jsFolderName,
+        config.destination.assetsFolder + config.destination.viewsFolderName,
+        config.destination.assetsFolder + config.destination.imagesFolderName
+    ]).then(() => cb(), () => { console.log('Error during clean task')});
+});
+
+/**
+ * Build app from sources
+ */
+gulp.task('build', ['clean'], function() {
+    return runSequence(['scripts', 'lib', 'fonts', 'images', 'fonticon', 'styles', 'views']);
+});
+
+//BrowserSync
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: config.vhost
+    });
+});
+
+/**
+ * Watch task for development
+ */
+gulp.task('watch', ['build'],  function() {
+    gulp.watch(config.source.jsFileList, ['scripts']);
+    gulp.watch(config.source.libFileList, ['lib']);
+    gulp.watch(config.source.fontFileList, ['fonts']);
+    gulp.watch(config.source.imageFileList, ['images']);
+    gulp.watch(config.source.fontIcon, ['fonticon']);
+    gulp.watch(config.source.cssWatchFileList, ['styles']);
+    gulp.watch(config.source.viewFileList, ['views']);
+});
+
+gulp.task('serve', ['build', 'watch', 'browser-sync']);
+gulp.task('default', ['build'], function () { });
